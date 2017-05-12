6c525300ff983823c2f4898e6e6ac684f76a0262
Ömer <omergulen@std.iyte.edu.tr>
2017-05-12 19:58:37 +0300

    Update v0.4.1.py
    bug fixes...

--- v0.4.1.py
+++ v0.4.1.py
@@ -5,11 +5,10 @@ import json
 import re
 import logging
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from urllib.request import urlopen
 from forex_python.converter import CurrencyRates
 from datetime import datetime
-#
+
 logging.basicConfig(filename='logs', filemode='a', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 
@@ -22,61 +21,68 @@ def comma_me(amount):
         return comma_me(new)
 
 
-async def control_currency_btc():
+def control_currency_btc():
     j = urlopen('https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=10')
     global j_obj
     j_obj = json.load(j)
 
     global c
     c = CurrencyRates()
-    
-async def control_currency_eth():
-    j = urlopen('https://api.coinmarketcap.com/v1/ticker/ethereum')
-    global j_obj
-    j_obj = json.load(j)
 
-    global c
-    c = CurrencyRates()
-    
-async def control_currency_ltc():
-    j = urlopen('https://api.coinmarketcap.com/v1/ticker/litecoin')
+
+def control_currency_eth():
+    j = urlopen('https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=EUR')
     global j_obj
     j_obj = json.load(j)
 
     global c
     c = CurrencyRates()
 
-async def control_currency_xrp():
-    j = urlopen('https://api.coinmarketcap.com/v1/ticker/ripple')
+
+def control_currency_ltc():
+    j = urlopen('https://api.coinmarketcap.com/v1/ticker/litecoin/?convert=EUR')
     global j_obj
     j_obj = json.load(j)
 
     global c
     c = CurrencyRates()
-    
-async def control_currency_etc():
-    j = urlopen('https://api.coinmarketcap.com/v1/ticker/ethereum-classic')
+
+
+def control_currency_xrp():
+    j = urlopen('https://api.coinmarketcap.com/v1/ticker/ripple/?convert=EUR')
     global j_obj
     j_obj = json.load(j)
 
     global c
     c = CurrencyRates()
-    
-async def control_currency_xmr():
-    j = urlopen('https://api.coinmarketcap.com/v1/ticker/monero')
+
+
+def control_currency_etc():
+    j = urlopen('https://api.coinmarketcap.com/v1/ticker/ethereum-classic/?convert=EUR')
     global j_obj
     j_obj = json.load(j)
 
     global c
     c = CurrencyRates()
-    
-async def control_currency_dash():
-    j = urlopen('https://api.coinmarketcap.com/v1/ticker/dash')
+
+
+def control_currency_xmr():
+    j = urlopen('https://api.coinmarketcap.com/v1/ticker/monero/?convert=EUR')
     global j_obj
     j_obj = json.load(j)
 
     global c
     c = CurrencyRates()
+
+
+def control_currency_dash():
+    j = urlopen('https://api.coinmarketcap.com/v1/ticker/dash/?convert=EUR')
+    global j_obj
+    j_obj = json.load(j)
+
+    global c
+    c = CurrencyRates()
+
 
 async def on_chat_message(msg):
     global chat_id
@@ -107,14 +113,14 @@ async def on_chat_message(msg):
                                        "BTC-USD Volume in 24h : {} USD\n"
                                        "BTC % Changed in 24h : {} %".format(
             datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            float(j_obj["price_usd"]),
-            float(j_obj["price_eur"]),
-            float(c.convert('USD', 'TRY', round(float(j_obj["price_usd"]), 2))),
-            comma_me(j_obj["24h_volume_usd"]),
-            comma_me(j_obj["percent_change_24h"])))
+            float(j_obj[0]["price_usd"]),
+            float(j_obj[0]["price_eur"]),
+            float(c.convert('USD', 'TRY', round(float(j_obj[0]["price_usd"]), 2))),
+            comma_me(j_obj[0]["24h_volume_usd"]),
+            comma_me(j_obj[0]["percent_change_24h"])))
 
     elif msg['text'] == "/ETC":
-        await control_currency_etc()
+        control_currency_etc()
 
         await bot.sendMessage(chat_id, "Etherium Classic @ {}\n"
                                        "1 ETC = {:.2f} USD\n"
@@ -123,14 +129,14 @@ async def on_chat_message(msg):
                                        "ETC-USD Volume in 24h : {} USD\n"
                                        "ETC % Changed in 24h : {} %".format(
             datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            float(j_obj["price_usd"]),
-            float(j_obj["price_eur"]),
-            float(c.convert('USD', 'TRY', round(float(j_obj["price_usd"]), 2))),
-            comma_me(j_obj["24h_volume_usd"]),
-            comma_me(j_obj["percent_change_24h"])))
+            float(j_obj[0]["price_usd"]),
+            float(j_obj[0]["price_eur"]),
+            float(c.convert('USD', 'TRY', round(float(j_obj[0]["price_usd"]), 2))),
+            comma_me(j_obj[0]["24h_volume_usd"]),
+            comma_me(j_obj[0]["percent_change_24h"])))
 
     elif msg['text'] == "/XRP":
-        await control_currency_xrp()
+        control_currency_xrp()
 
         await bot.sendMessage(chat_id, "Ripple @ {}\n"
                                        "1 XRP = {:.2f} USD\n"
@@ -139,14 +145,14 @@ async def on_chat_message(msg):
                                        "XRP-USD Volume in 24h : {} USD\n"
                                        "XRP % Changed in 24h : {} %".format(
             datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            float(comma_me(j_obj["price_usd"])),
-            float(comma_me(j_obj["price_eur"])),
-            float(c.convert('USD', 'TRY', round(float(j_obj["price_usd"]), 2))),
-            comma_me(j_obj["24h_volume_usd"]),
-            comma_me(j_obj["percent_change_24h"])))
+            float(comma_me(j_obj[0]["price_usd"])),
+            float(comma_me(j_obj[0]["price_eur"])),
+            float(c.convert('USD', 'TRY', round(float(j_obj[0]["price_usd"]), 2))),
+            comma_me(j_obj[0]["24h_volume_usd"]),
+            comma_me(j_obj[0]["percent_change_24h"])))
 
     elif msg['text'] == "/ETH":
-        await control_currency_eth()
+        control_currency_eth()
 
         await bot.sendMessage(chat_id, "Ethereum @ {}\n"
                                        "1 ETH = {:.2f} USD\n"
@@ -155,14 +161,14 @@ async def on_chat_message(msg):
                                        "ETH-USD Volume in 24h : {} USD\n"
                                        "ETH % Changed in 24h : {} %".format(
             datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            float(comma_me(j_obj["price_usd"])),
-            float(comma_me(j_obj["price_eur"])),
-            float(c.convert('USD', 'TRY', round(float(j_obj["price_usd"]), 2))),
-            comma_me(j_obj["24h_volume_usd"]),
-            comma_me(j_obj["percent_change_24h"])))
+            float(comma_me(j_obj[0]["price_usd"])),
+            float(comma_me(j_obj[0]["price_eur"])),
+            float(c.convert('USD', 'TRY', round(float(j_obj[0]["price_usd"]), 2))),
+            comma_me(j_obj[0]["24h_volume_usd"]),
+            comma_me(j_obj[0]["percent_change_24h"])))
 
     elif msg['text'] == "/LTC":
-        await control_currency_ltc()
+        control_currency_ltc()
 
         await bot.sendMessage(chat_id, "Litecoin @ {}\n"
                                        "1 LTC = {:.2f} USD\n"
@@ -171,14 +177,14 @@ async def on_chat_message(msg):
                                        "LTC-USD Volume in 24h : {} USD\n"
                                        "LTC % Changed in 24h : {} %".format(
             datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            float(comma_me(j_obj["price_usd"])),
-            float(comma_me(j_obj["price_eur"])),
-            float(c.convert('USD', 'TRY', round(float(j_obj["price_usd"]), 2))),
-            comma_me(j_obj["24h_volume_usd"]),
-            comma_me(j_obj["percent_change_24h"])))
+            float(comma_me(j_obj[0]["price_usd"])),
+            float(comma_me(j_obj[0]["price_eur"])),
+            float(c.convert('USD', 'TRY', round(float(j_obj[0]["price_usd"]), 2))),
+            comma_me(j_obj[0]["24h_volume_usd"]),
+            comma_me(j_obj[0]["percent_change_24h"])))
 
     elif msg['text'] == "/DASH":
-        await control_currency_dash()
+        control_currency_dash()
 
         await bot.sendMessage(chat_id, "Dash @ {}\n"
                                        "1 DASH = {:.2f} USD\n"
@@ -187,14 +193,14 @@ async def on_chat_message(msg):
                                        "DASH-USD Volume in 24h : {} USD\n"
                                        "DASH % Changed in 24h : {} %".format(
             datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            float(comma_me(j_obj["price_usd"])),
-            float(comma_me(j_obj["price_eur"])),
-            float(c.convert('USD', 'TRY', round(float(j_obj["price_usd"]), 2))),
-            comma_me(j_obj["24h_volume_usd"]),
-            comma_me(j_obj["percent_change_24h"])))
+            float(comma_me(j_obj[0]["price_usd"])),
+            float(comma_me(j_obj[0]["price_eur"])),
+            float(c.convert('USD', 'TRY', round(float(j_obj[0]["price_usd"]), 2))),
+            comma_me(j_obj[0]["24h_volume_usd"]),
+            comma_me(j_obj[0]["percent_change_24h"])))
 
     elif msg['text'] == "/XMR":
-        await control_currency_xmr()
+        control_currency_xmr()
 
         await bot.sendMessage(chat_id, "Monero @ {}\n"
                                        "1 XMR = {:.2f} USD\n"
@@ -203,11 +209,11 @@ async def on_chat_message(msg):
                                        "XMR-USD Volume in 24h : {} USD\n"
                                        "XMR % Changed in 24h : {} %".format(
             datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            float(comma_me(j_obj["price_usd"])),
-            float(comma_me(j_obj["price_eur"])),
-            float(c.convert('USD', 'TRY', round(float(j_obj["price_usd"]), 2))),
-            comma_me(j_obj["24h_volume_usd"]),
-            comma_me(j_obj["percent_change_24h"])))
+            float(comma_me(j_obj[0]["price_usd"])),
+            float(comma_me(j_obj[0]["price_eur"])),
+            float(c.convert('USD', 'TRY', round(float(j_obj[0]["price_usd"]), 2))),
+            comma_me(j_obj[0]["24h_volume_usd"]),
+            comma_me(j_obj[0]["percent_change_24h"])))
 
     elif msg['text'] == "/about":
         await bot.sendMessage(chat_id, "To advice us:\n"
@@ -222,7 +228,7 @@ async def on_chat_message(msg):
                                        '/about to advice us :))')
 
 
-TOKEN = "************************************"
+TOKEN =  "**************************************"
 
 bot = telepot.aio.Bot(TOKEN)
 answerer = telepot.aio.helper.Answerer(bot)
